ec2866e35e664a8755124c65a666e256422b3831
zeeraozdemir <zeeraozdemir@gmail.com>
2023-02-02 22:19:55 +0300

    güncellendi

--- veri_tipi_dönüsümleri.py
+++ veri_tipi_dönüsümleri.py
@@ -1,9 +1,30 @@
-pi = 3.14
-
-r  = float( input("Yarı cap: "))
-
-alan = pi * (r ** 2)
-cevre = 2 *  pi * r
-
-print("alan", alan)
-print("cevre",cevre)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)]"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "f0e6a655b6036305b310326d5b94fe4f588858eb6b0ad9b0044624012cfc47cf"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
